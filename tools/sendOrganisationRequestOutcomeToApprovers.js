873ec06447e5cbc590f22603da383727a1545dd7
Adam Mann <Adam.Mann@education.gov.uk>
2025-09-23 10:43:18 +0100

    Modify test data

--- tools/sendOrganisationRequestOutcomeToApprovers.js
+++ tools/sendOrganisationRequestOutcomeToApprovers.js
@@ -1,15 +1,16 @@
 const utils = require("./utils");
 
+// Need to fill in variables with real data
 utils
   .getClient()
   .then((client) => {
     client
       .sendOrganisationRequestOutcomeToApprovers(
-        "6bea40ae-947d-4767-9a97-c52fced78b33",
-        "adam.mann+3@education.gov.uk",
-        "Adam Mann",
-        "faa4a47a-0d26-4eb5-8be5-11ccb53ad801", //Cardiff Academy
-        "Cardiff Academy",
+        "an-approver-uuid",
+        "end-user-email@test.gov.uk",
+        "End User Name",
+        "an-organisation-uuid",
+        "Organisation Name",
         true,
         null,
       )
